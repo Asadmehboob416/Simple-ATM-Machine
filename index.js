711ea20fceede3fe4ad968359eb604aac7574027
Asad Mehboob <163368482+Asadmehboob416@users.noreply.github.com>
2024-04-01 03:29:39 +0500

    ATM machine
    it is  a simple ATM machine which have 2 feature .

---
+++ index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 1331;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: 'pin',
+        message: 'Enter Your Pin : ',
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Correct Pin!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Please Select Option",
+            type: "list",
+            choices: ["Cash With Draw", "Check Balance"]
+        }
+    ]);
+    console.log("operation");
+    if (operationAns.operation === "Cash With Draw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter Your Amount",
+                type: "number"
+            }
+        ]);
+        myBalance -= amountAns.amount;
+        console.log("Your Remaining BAlance is : " + myBalance);
+    }
+    else if (operationAns.operation === "Check Balance") {
+        console.log("My Balance is : " + myBalance);
+    }
+}
+else {
+    console.log("Incorrect Pin (Please Type Correct Pin)");
+}
+;
